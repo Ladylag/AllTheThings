bdf5faafc336c30dfe4f4d94532db10373f87891
Dmitry Volodin <mr.molkree@gmail.com>
2022-05-03 03:45:39 +0400

    Get rid of intermediate files

--- .contrib/Harvesters/MissingQuestsHarvester.py
+++ .contrib/Harvesters/MissingQuestsHarvester.py
@@ -4,44 +4,31 @@ import requests
 
 DB_FILE = "db/Categories.lua"
 BUILDS_FILE = ".contrib/Harvesters/builds.txt"
-USED_QUESTS_FILE = "quest_ids.txt"
-BUILD_QUESTS_FILE = "build_quests.txt"
 DIFF_FILE = "diff.txt"
 
 
-def get_used_quest_ids() -> None:
+def get_used_quest_ids() -> set[str]:
     quest_ids = set[str]()
     with open(DB_FILE) as categories_lua:
         for line in categories_lua:
             for element in line.split(","):
                 if match := re.search(r"(?:q\(|questID=)(\d+)", element):
                     quest_ids.add(match.group(1))
-    with open(USED_QUESTS_FILE, "w") as quest_ids_file:
-        quest_ids_file.write("\n".join(sorted(quest_ids, key=int)))
-        quest_ids_file.write("\n")
+    return quest_ids
 
 
-def get_build_quests(build: str) -> None:
+def get_build_quests(build: str) -> set[str]:
     url = f"https://wow.tools/dbc/api/export/?name=questv2&build={build}"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36"
     }
     request = requests.get(url, headers=headers)
     lines = request.text.splitlines()[1:]
-    with open(BUILD_QUESTS_FILE, "w") as build_quests_file:
-        quests = map(lambda x: x.split(",")[0], lines)
-        build_quests_file.write("\n".join(quests))
-        build_quests_file.write("\n")
+    return {line.split(",")[0] for line in lines}
 
 
-def build_diff(build: str) -> None:
-    with (
-        open(USED_QUESTS_FILE) as used_quests_file,
-        open(BUILD_QUESTS_FILE) as build_quests_file,
-        open(DIFF_FILE) as diff_file,
-    ):
-        used_quests = set(used_quests_file.read().splitlines())
-        build_quests = set(build_quests_file.read().splitlines())
+def build_diff(used_quests: set[str], build: str, build_quests: set[str]) -> None:
+    with open(DIFF_FILE) as diff_file:
         diff_stuff = set(diff_file.read().splitlines())
     difference = build_quests - used_quests - diff_stuff
     if difference:
@@ -52,13 +39,13 @@ def build_diff(build: str) -> None:
 
 
 def main() -> None:
-    get_used_quest_ids()
+    used_quest_ids = get_used_quest_ids()
     open(DIFF_FILE, "w").close()
     build_list = open(BUILDS_FILE).read().splitlines()
     for build in reversed(build_list):
         print(build)
-        get_build_quests(build)
-        build_diff(build)
+        build_quests = get_build_quests(build)
+        build_diff(used_quest_ids, build, build_quests)
 
 
 main()
@@ -117,4 +104,4 @@ def CheckMissing():
                 DiffMissing.write(TOOLline + "\n")
 
 
-CheckMissing()
+# CheckMissing()
