92d1461020942bb6305e9c7e20af3cd494075914
Dmitry Volodin <mr.molkree@gmail.com>
2022-05-03 04:29:46 +0400

    Build better representation of missing quests

--- .contrib/Harvesters/MissingQuestsHarvester.py
+++ .contrib/Harvesters/MissingQuestsHarvester.py
@@ -1,10 +1,11 @@
 import re
 
 import requests
+import os
 
 DB_FILE = "db/Categories.lua"
 BUILDS_FILE = ".contrib/Harvesters/builds.txt"
-DIFF_FILE = "diff.txt"
+MISSING_QUESTS_FILE = "diff.txt"
 
 
 def get_quest_ids(filepath: str) -> set[str]:
@@ -27,25 +28,27 @@ def get_build_quests(build: str) -> set[str]:
     return {line.split(",")[0] for line in lines}
 
 
-def build_diff(used_quests: set[str], build: str, build_quests: set[str]) -> None:
-    with open(DIFF_FILE) as diff_file:
-        diff_stuff = set(diff_file.read().splitlines())
-    difference = build_quests - used_quests - diff_stuff
-    if difference:
-        with open(DIFF_FILE, "a") as diff_file:
-            diff_file.write(build + "\n")
-            diff_file.write("\n".join(sorted(difference, key=int)))
-            diff_file.write("\n")
+def get_missing_quests(used_quests: set[str], build_quests: set[str]) -> set[str]:
+    missing_quests = build_quests - used_quests
+    if os.path.exists(MISSING_QUESTS_FILE):
+        missing_quests -= get_quest_ids(MISSING_QUESTS_FILE)
+    return missing_quests
 
 
-def main() -> None:
+def build_missing_quests() -> None:
     used_quest_ids = get_quest_ids(DB_FILE)
-    open(DIFF_FILE, "w").close()
     build_list = open(BUILDS_FILE).read().splitlines()
     for build in reversed(build_list):
         print(build)
         build_quests = get_build_quests(build)
-        build_diff(used_quest_ids, build, build_quests)
+        missing_quests = get_missing_quests(used_quest_ids, build_quests)
+        if missing_quests:
+            with open(MISSING_QUESTS_FILE, "a") as missing_quests_file:
+                missing_quests_file.write(build + "\nq(")
+                missing_quests_file.write(
+                    "),\t--\nq(".join(sorted(missing_quests, key=int))
+                )
+                missing_quests_file.write("),\t--\n")
 
 
-main()
+build_missing_quests()
